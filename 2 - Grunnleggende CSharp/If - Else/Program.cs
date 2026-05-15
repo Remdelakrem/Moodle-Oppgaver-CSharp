791b6cce28b87c0640c6b8df74aabfc33accee48
RemyDale <remydale@hotmail.com>
2026-05-15 14:49:33 +0200

    If - Else, og Random Hobby generator

---
+++ 2 - Grunnleggende CSharp/If - Else/Program.cs
@@ -0,0 +1,58 @@
+﻿// Oppgaver om conditions - if/else
+// Oppgave 1: 
+// Lag en metode som tar imot to tall og returnerer true dersom tallene er like.
+
+
+//  int liteTall = 2;
+//  int størreTall = 5;
+//  if(liteTall < størreTall) // her er betingelsen at liteTall skal være mindre enn størreTall
+//  {
+//      // if kan dermed leses som dersom liteTall mindre enn størreTall
+//      // utfør kode inne i klammene
+//  }
+//  else 
+//  {
+//      // dersom betingelsen i parentesen på if-setningen ikke er sann 
+//      //( dersom liteTall ikke er mindre enn størreTall)
+//      //  skal man heller utføre koden som finnes her inne
+//  }
+
+
+// Oppgave 2: 
+// Lag en metode som tar imot to tall og returnerer summen av tallene dersom de er ulike, 
+// og returnerer tallene multiplisert med hverandre dersom de er like
+
+
+// Console.WriteLine(Oppgave2(6, 6));
+// Console.WriteLine(Oppgave2(10, 13));
+//    
+// return;
+//    
+// int Oppgave2(int a, int b)
+// {
+//     if (a == b) return a * b;
+//    
+//     return a + b;
+// }
+
+
+// Oppgave 3:
+// Lag en metode som tar inn to int verdier. Dersom en av de, eller summen til int verdiene blir 30,
+// skal metoden returnere true. Ellers returnerer metoden false
+
+// Console.WriteLine(Oppgave3(15, 15));
+// Console.WriteLine(Oppgave3(28, 10));
+// Console.WriteLine(Oppgave3(30, 0));
+// Console.WriteLine(Oppgave3(0, 30));
+// 
+// return;
+// 
+// bool Oppgave3(int a, int b)
+// {
+//     if (a + b == 30) return true;
+//     if (a == 30 || b == 30) return true;
+// 
+//     return false;
+// }
+
+
---
+++ 2 - Grunnleggende CSharp/Random i CSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Ønsker du en ny hobby? Skriv et navn under!");
+var name = Console.ReadLine();
+
+var hobby0 = "Fotball";
+var hobby1 = "Trylling";
+var hobby2 = "Discgolf";
+var hobby3 = "Bowling";
+var hobby4 = "Gitar";
+
+var rand = new Random();
+var randomHobby = rand.Next(0, 5);
+if (randomHobby == 0)
+    Console.WriteLine($"Javell.. {name}, Da ble det {hobby0} på deg!");
+else if (randomHobby == 1)
+    Console.WriteLine($"Javell.. {name}, Da ble det {hobby1} på deg!");
+else if (randomHobby == 2)
+    Console.WriteLine($"Javell.. {name}, Da ble det {hobby2} på deg!");
+else if (randomHobby == 3)
+    Console.WriteLine($"Javell.. {name}, Da ble det {hobby3} på deg!");
+else if (randomHobby == 4) Console.WriteLine($"Javell.. {name}, Da ble det {hobby4} på deg!");
